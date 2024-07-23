fcb8b2619f73fc91d14d88274afcdd0d557d3b7b
chrissys <chrissys@student.21-school.ru>
2024-07-23 21:53:36 +0300

    check

--- src/bit_operations/s21_scale.c
+++ src/bit_operations/s21_scale.c
@@ -5,15 +5,13 @@ int s21_get_scale(s21_decimal value){
     s21_set_sign(&result,0);
     
     return result.bits[3] >> 16;
-
 }
 
 int s21_set_scale(s21_decimal *value, int scale){
     bool result = scale >= 0 && scale <= 28;
     if (result){
         int sign = s21_get_bit(*value, 127);
-        value->bits[3] = scale;
-        value->bits[3] = value->bits[3] << 16;
+        value->bits[3] |= scale << 16;
         s21_set_sign(value, sign);
     }
     return !result;
--- src/main.c
+++ src/main.c
@@ -9,25 +9,14 @@ void bitwise_shift_memory(void* memory, size_t size, int shift) {
 }
 
 int main() {
-//   s21_decimal* value = s21_create_decimal();
-//   s21_set_sign(value, 1);
-//   s21_set_scale(value, 28);
- 
-//  s21_set_bit(value, 31);
-
-//   s21_pretty_print_bits(*value);
-//   value->bits[0] = value->bits[0] >> 1;
-//   s21_pretty_print_bits(*value);
-//   s21_free_decimal(value);
-
-  s21_big_decimal* value = s21_big_create_decimal();
+  s21_decimal* value = s21_create_decimal();
   
-  s21_big_set_sign(value, 1);
-  s21_big_set_scale(value, 28);
+  s21_set_sign(value, 1);
+  s21_set_scale(value, 28);
  
-  s21_big_pretty_print_bits(*value);
+  s21_pretty_print_bits(*value);
  
-  s21_big_free_decimal(value);
+  s21_free_decimal(value);
  
   return 0;
 }
